ce7a0a2af892f2c9efc0075fdf3b55f32b3e7a43
MITREXid <seryjgrom24222@gmail.com>
2026-01-31 13:29:07 +0300

    Магистраль: примерная логика рбаоты 1ой версии

--- magistral.hpp
+++ magistral.hpp
@@ -7,10 +7,10 @@
 enum state_Magistral {
     default_ = 0,//в нем стоим пока не запустимя
     going_to_gate = 5,//высыпаем зерно в шлюз
-    all_close = 1,//все закрто
+    // all_close = 1,//все закрто
     in_magistral = 2,//в этом стотоянии ждем пока всё высыыпется в трубу
     air_on = 4,//в этом состоянии мы открываем шаровый (закрываем всё остальное) и ждем пока зерно дойдет до конца магистали
-    full_open = 11//режим прочистки
+    // full_open = 11//режим прочистки
 };
 
 // enum status_Magistral{
@@ -35,7 +35,6 @@ private:
     struct _times_turn_to{
         // uint32_t default_ = 0;//из него выходм только по стрту
         uint32_t going_to_gate = 700;
-        uint32_t all_close = 1300;
         uint32_t in_magistral = 2000;
         uint32_t air_on = 22000;
         // uint32_t full_open = 1300;//не известно нужно ли время
@@ -45,7 +44,7 @@ public:
     Magistral(Shared_power_5V &PWM_, int control_pin_actuator, int control_pin_clapan, int control_pin_ball_cran)
     {
         actuator = new Actuator(state_Component::close, PWM_, control_pin_actuator);
-        clapan = new Clapan(state_Component::close, PWM_, control_pin_clapan);
+        clapan = new Clapan(state_Component::open, PWM_, control_pin_clapan);
         ball_cran = new Ball_cran(state_Component::close, control_pin_ball_cran);
         data.state = state_Magistral::default_;
     }
@@ -60,38 +59,31 @@ public:
 
 void Magistral::turn_to(state_Magistral next_state){
     switch(next_state){
-        case state_Magistral::default_://всё закрыто поумолчанию
+        case state_Magistral::default_://актуатор закрыт клапан открыт шаровой кран закрыт по умолчанию
                 actuator->close();
-                clapan->close();
+                clapan->open();
                 ball_cran->close();
                 data.state = state_Magistral::default_;
             break;
-        case state_Magistral::in_magistral://актуатор закрыт клапан открыт шаровой кран закрыт
-                clapan->open();
-                ball_cran->close();
-                actuator->close();
-                data.state = state_Magistral::in_magistral;
-            break;
-        case state_Magistral::all_close://актуатор закрыт клапан закрыт шаровой кран закрыт
-                clapan->close();
-                ball_cran->close();
-                actuator->close();
-            data.state = state_Magistral::all_close;
-            break;
-        case state_Magistral::air_on://актуатор закрыт клапан закрыт шаровой кран открыт
-                actuator->close();
-                clapan->close();
-                ball_cran->open();
-            data.state = state_Magistral::air_on;
-            break;
-        case state_Magistral::going_to_gate://актуатор открыт клапан закрыт шаровой кран закрыт
+         case state_Magistral::going_to_gate://актуатор открыт клапан закрыт шаровой кран закрыт
                 actuator->open();
                 clapan->close();
                 ball_cran->close();
             data.state = state_Magistral::going_to_gate;
             break;
-        case state_Magistral::full_open:
-            println("not implemented full_open");
+        case state_Magistral::in_magistral://актуатор закрыт клапан открыт шаровой кран закрыт
+                actuator->close();
+                clapan->open();
+                ball_cran->close();
+                data.state = state_Magistral::in_magistral;
+            break;
+        case state_Magistral::air_on://актуатор закрыт клапан закрыт шаровой кран открыт
+                actuator->close();
+                clapan->open();
+                // actuator->open(); //почему мы так не делаем не понятно
+                // clapan->close();
+                ball_cran->open();
+            data.state = state_Magistral::air_on;
             break;
     };
 }
@@ -160,15 +152,17 @@ void Magistral::logic(){
     if(data.state == state_Magistral::going_to_gate && times_turn_to.going_to_gate <cur_time){
         print("=========");
         print(cur_time);
-        println("=====turn_to(state_Magistral::all_close);");
-        turn_to(state_Magistral::all_close);
-    }
-    if(data.state == state_Magistral::all_close && times_turn_to.all_close <cur_time){
-        print("=========");
-        print(cur_time);
-        println("=========turn_to(state_Magistral::in_magistral);");
+        println("=====turn_to(state_Magistral::in_magistral);");
         turn_to(state_Magistral::in_magistral);
+        // println("=====turn_to(state_Magistral::all_close);");
+        // turn_to(state_Magistral::all_close);
     }
+    // if(data.state == state_Magistral::all_close && times_turn_to.all_close <cur_time){
+    //     print("=========");
+    //     print(cur_time);
+    //     println("=========turn_to(state_Magistral::in_magistral);");
+    //     turn_to(state_Magistral::in_magistral);
+    // }
     if(data.state == state_Magistral::in_magistral && times_turn_to.in_magistral<cur_time){
         print("=========");
         print(cur_time);
