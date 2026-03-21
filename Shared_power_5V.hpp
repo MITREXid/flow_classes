96a9ddd85303377522a4afb5fef99592df8ca6e9
lnoprshelenec <fedorov4.alisa@yandex.ru>
2026-03-21 16:33:11 +0300

    Revert "Удален Shared_power_5V"
    This reverts commit 7bdea4f11818980818235e6470f9ab83807089b1.

---
+++ Shared_power_5V.hpp
@@ -0,0 +1,59 @@
+#ifndef SHARED_POWER_5V_HPP
+#define SHARED_POWER_5V_HPP
+#include "universal_object.hpp"
+
+class Shared_power_5V{//здесь статус это число тех кому ещё нужно питание
+private:
+    int state=0;
+    char control_pin = -1;
+public:
+    Shared_power_5V(char control_pin_);
+    void voltageON();
+    void voltageOFF();
+    void update();
+    void setStatus(int status);
+    int getStatus();
+
+};
+
+Shared_power_5V::Shared_power_5V(char control_pin_){
+    control_pin = control_pin_;
+    setStatus(0);
+    pinMode(control_pin, OUTPUT);
+}
+
+void Shared_power_5V::voltageON()
+{
+    pinMode(control_pin, HIGH);
+    setStatus(state+1);
+}
+
+void Shared_power_5V::voltageOFF()
+{
+    setStatus(state-1);
+}
+
+void Shared_power_5V::update()
+{
+    static bool flag = 0;
+    if(getStatus()==0 && flag){
+        pinMode(control_pin, LOW);
+        flag = 0;
+    }
+    if(getStatus()>0){
+        flag = 1;
+    }
+}
+
+void Shared_power_5V::setStatus(int status){
+    if(getStatus()<0){
+        d_println(F("Error Shared_power_5V status < 0"));
+        status = 0;
+    }
+    state = status;
+}
+
+int Shared_power_5V::getStatus(){
+    return state;
+}
+#endif // SHARED_POWER_5V_HPP
